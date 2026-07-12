21a8fdc2d3e3cd8ca03381d410fd2cc05fa47c13
Vansh <vanshvijay9784@gmail.com>
2026-07-12 08:38:50 +0530

    build: configure consistpay.tech domains in CORS allowedOrigins and add SEO public assets

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -10,7 +10,9 @@ const allowedOrigins = [
   "http://localhost:5173", 
   "http://localhost:5174", 
   "http://localhost:5175",
-  "https://daily-coding-habit-tracker.vercel.app"
+  "https://daily-coding-habit-tracker.vercel.app",
+  "https://consistpay.tech",
+  "https://www.consistpay.tech"
 ];
 
 app.use(cors({
